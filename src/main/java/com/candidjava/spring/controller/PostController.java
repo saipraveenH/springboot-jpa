4b546661bd825f9bdf51d38de8627059bb82590a
praveen.hemadri <praveen.hemadri@user-265.otsi.com>
2019-10-28 18:57:39 +0530

    Added post controller.

---
+++ src/main/java/com/candidjava/spring/controller/PostController.java
@@ -0,0 +1,10 @@
+package com.candidjava.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = { "/post" })
+public class PostController {
+
+}
